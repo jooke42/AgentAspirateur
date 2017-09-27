91295f235a0e36bf9e43ffa172246dbcef48be7f
Alexandre Marteaux <ialexandre.marteaux@gmail.com>
2017-09-26 21:52:19 -0400

    first commit

---
+++ AgentAspirateur/Environment.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+
+namespace AgentAspirateur
+{
+    enum Tile { CLEAN_FLOOR,DIRTY_FLOOR, JEWELRY_FLOOR}
+
+    public struct Size
+    {
+        public int width, height;
+    }
+
+    class Environment
+    {
+        private Tile[,] map;
+        int[] robotPosition;
+        Random rnd = new Random();
+
+        public Environment()
+        {
+            init();
+        }
+
+        public void init()
+        {
+            this.map = new Tile[10,10];
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = Tile.CLEAN_FLOOR;
+                }
+            }
+
+        }
+        public void start()
+        {
+            while (true)
+            {
+                Thread.Sleep(2000);
+                generateRandom(Tile.DIRTY_FLOOR, 0, 2);
+                
+            }
+        }
+        public void generateRandom(Tile tile,int min, int max)
+        {
+            int nbToCreate = rnd.Next(min, max);
+            while(nbToCreate > 0)
+            {
+                int x = rnd.Next(0, 10);
+                int y = rnd.Next(0, 10);
+                if(map[x,y] == Tile.CLEAN_FLOOR)
+                {
+                    map[x, y] = tile;
+                    nbToCreate--;
+                    Console.WriteLine("generating dirt at (" + x + ";" + y + ")");
+                }
+            }
+        }
+
+        public Tile[,] getMap()
+        {
+            return this.map;
+        }
+        
+        
+        public string toString()
+        {
+            string s = "";
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                s += "|";
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    s += "\t" + map[i, j] + "\t|";
+                }
+            }
+            return s;
+        }
+
+
+    }
+}
--- AgentAspirateur/MainWindow.xaml.cs
+++ AgentAspirateur/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+using System.Timers;
 
 namespace AgentAspirateur
 {
@@ -20,9 +23,93 @@ namespace AgentAspirateur
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Environment environment;
+        private readonly System.Timers.Timer _timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            environment = new Environment();
+            ThreadStart environmentThreadRef = new ThreadStart(environment.start);
+            Console.WriteLine("In Main: Creating the Environment thread");
+            Thread environmentThread = new Thread(environmentThreadRef);
+            environmentThread.Start();
+
+            _timer = new System.Timers.Timer(2000); //Updates every quarter second.
+            _timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            _timer.Start();
+            
+        
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+
+            this.Dispatcher.Invoke(DispatcherPriority.Normal,
+            new TimerDispatcherDelegate(Update));
+            
+        }
+        private delegate void TimerDispatcherDelegate();
+
+
+        private void Update()
+        {
+            int x, y;
+            // Simulate some work taking place
+            //Thread.Sleep(TimeSpan.FromSeconds(5));
+            Tile[,] map = environment.getMap();
+            DrawingGroup imageDrawings = new DrawingGroup();
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    x = i * 64;
+                    y = j * 64;
+                    ImageDrawing tile = new ImageDrawing();
+                    tile.Rect = new Rect(x, y, 64, 64);
+                    tile.ImageSource = new BitmapImage(
+                        new Uri(@"Ressources\sol_parquet_with_border.png", UriKind.Relative));
+                    switch (map[i, j])
+                    {
+                        case Tile.DIRTY_FLOOR:
+                            Console.WriteLine("drawing dirty floor at ("+i+";"+j+")");
+                            tile.ImageSource = new BitmapImage(
+                        new Uri(@"Ressources\dust.png", UriKind.Relative));
+                            break;
+                        case Tile.JEWELRY_FLOOR:
+                            tile.ImageSource = new BitmapImage(
+                        new Uri(@"Ressources\diamond.png", UriKind.Relative));
+                            break;
+                    }
+                    
+                    imageDrawings.Children.Add(tile);
+                    
+                }
+            }
+            //
+            // Use a DrawingImage and an Image control to
+            // display the drawings.
+            //
+            DrawingImage drawingImageSource = new DrawingImage(imageDrawings);
+
+            // Freeze the DrawingImage for performance benefits.
+            drawingImageSource.Freeze();
+
+            mapImage.BeginInit();
+            mapImage.Source = null;
+            mapImage.EndInit();
+            mapImage.BeginInit();
+            mapImage.Stretch = Stretch.None;
+            mapImage.Source = drawingImageSource;
+            
+            mapImage.EndInit();
+
+        }
+        private void bitmapCoord(int i,int j,out int x, out int y)
+        {
+            x = i * 64;
+            y = i * 64;
         }
     }
+    
 }
