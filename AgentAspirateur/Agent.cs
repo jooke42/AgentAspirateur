d77986e760ea67aab292323f455ab64ac4899f22
Alexandre Marteaux <ialexandre.marteaux@gmail.com>
2017-10-01 14:41:22 -0400

    new env structure

--- AgentAspirateur/Agent.cs
+++ AgentAspirateur/Agent.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace AgentAspirateur
 {
+    enum Action { HAUT,BAS,GAUCHE,DROITE,ASPIRER,RAMASSER}
+
     class Agent
     {
+        int[,] beliefs;
+        List<Action> intents = new List<Action>();
+        int[,] desires;
         private Random rdm= new Random();
+
+        public void addRandomMovingAction()
+        {
+            Action[] movements = { Action.HAUT, Action.BAS, Action.GAUCHE, Action.DROITE };
+            intents.Add(movements[rdm.Next(4)]);
+        }
+
 
        
     }
