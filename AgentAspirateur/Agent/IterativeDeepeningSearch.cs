0974c327f4b424bc5a4d7fcae97b954b30f461f4
Maxence <gonfalonemaxence@gmail.com>
2017-10-03 18:01:22 +0200

    Iterative deepning search

---
+++ AgentAspirateur/Agent/IterativeDeepeningSearch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace AgentAspirateur.Agent
+{
+
+    
+
+    class IterativeDeepeningSearch : ITree
+    {
+
+        public const bool NOT_EMPTY = true;
+
+
+        Node ITree.SearchPath(Node nod) {
+
+            int detph = 0;
+            Node result = null;
+
+            for (detph = 0; detph < int.MaxValue; detph++)
+            {
+                result = DepthLimitedSearch(nod,NOT_EMPTY, detph);
+                if (result != null) return result;
+
+            }
+
+            return null;
+
+
+        }
+
+
+
+
+        Node DepthLimitedSearch(Node nod, bool goal, int limit)
+        {
+
+            Node result = null;
+            if (nod.hasDust() == goal || nod.hasDiamond() == goal) { return result; }
+            else if (limit == 0) { return result; }
+            else {
+                result = DepthLimitedSearch(nod.getNorth(), goal, --limit);
+
+                result = DepthLimitedSearch(nod.getSouth(), goal, --limit);
+                result = DepthLimitedSearch(nod.getWest(), goal, --limit);
+                result = DepthLimitedSearch(nod.getEast(), goal, --limit);
+
+
+
+
+            }
+
+            return null;
+
+        }
+
+    }
+}
---
+++ AgentAspirateur/Agent/Tree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentAspirateur.Agent
+{
+    interface ITree
+    {
+        Node SearchPath(Node nod);
+
+    }
+}
